fbf0e8beb6bf1222983568c276c65a780050dbda
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 11:04:54 +0530

    added SpringApplicationBuilder method

--- JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
+++ JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
@@ -2,12 +2,17 @@ package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class JenkinsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(JenkinsTestApplication.class);
 	}
 
 }
