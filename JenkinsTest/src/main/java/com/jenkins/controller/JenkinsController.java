aea8e9c44705962288c9256a25a551d5556a6951
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 18:21:13 +0530

    Added Post method

--- JenkinsTest/src/main/java/com/jenkins/controller/JenkinsController.java
+++ JenkinsTest/src/main/java/com/jenkins/controller/JenkinsController.java
@@ -2,6 +2,8 @@ package com.jenkins.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +15,11 @@ public class JenkinsController {
 		
 		return "Hi Jenkins...";
 
+	}
+	
+	@PostMapping("/post")
+	public String postMethod(@RequestBody String name) {
+return "Hi "+name+" post method is under implementation";
 	}
 
 }
