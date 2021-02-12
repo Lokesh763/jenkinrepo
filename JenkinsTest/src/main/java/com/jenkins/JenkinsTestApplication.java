81af1e5946c40af37e94ccbf0e3a6f74fbf2c767
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 12:48:29 +0530

    SpringBootServletInitializer - extended

--- JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
+++ JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
@@ -3,14 +3,14 @@ package com.jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication extends ServletInitializer {
+public class JenkinsTestApplication extends SpringBootServletInitializer {
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		// TODO Auto-generated method stub
-		return super.configure(application);
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 
 	public static void main(String[] args) {
