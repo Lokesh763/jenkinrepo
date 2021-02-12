78f528c5d1f6839395bdf9d30424c111bde66e51
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 12:21:12 +0530

    Old build

--- JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
+++ JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
@@ -1,23 +1,15 @@
 package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-public class JenkinsTestApplication extends ServletInitializer {
+public class JenkinsTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	    return application.sources(JenkinsTestApplication.class);
-	}
+	
 
 }
