21ff3a7e46a48e85d16b52f6cc84e1bbc36ec94e
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 10:53:50 +0530

    added get

--- JenkinsTest/src/main/java/com/jenkins/controller/JenkinsController.java
+++ JenkinsTest/src/main/java/com/jenkins/controller/JenkinsController.java
@@ -1,12 +1,12 @@
 package com.jenkins.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JenkinsController {
 	
-	@RequestMapping("/")
+	@GetMapping("/hello")
 	public String sayHello() {
 		
 		return "Hi Jenkins..";
