c6885767df30b4380723ef4dfc0d16c37269f070
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 12:41:50 +0530

    Changed code in ServletInitializer

--- JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
+++ JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
@@ -2,9 +2,16 @@ package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class JenkinsTestApplication {
+public class JenkinsTestApplication extends ServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// TODO Auto-generated method stub
+		return super.configure(application);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
