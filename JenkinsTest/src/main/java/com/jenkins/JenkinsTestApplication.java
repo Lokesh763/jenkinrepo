1bd74104b03f02b10870eb1c8f3f7187ebc4197b
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 12:00:31 +0530

    changed main class

--- JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
+++ JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
@@ -4,20 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-public class JenkinsTestApplication extends SpringBootServletInitializer {
+public class JenkinsTestApplication extends ServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
 	
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	    return application.sources(JenkinsTestApplication.class);
 	}
