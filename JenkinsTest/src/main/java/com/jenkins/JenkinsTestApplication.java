4423eeac0b8d226391a8d29b33760d0686eb88e9
MITSIND763 <MITSIND763@host.docker.internal>
2021-02-12 11:30:06 +0530

    Added Buddy dependency in pom.xml

--- JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
+++ JenkinsTest/src/main/java/com/jenkins/JenkinsTestApplication.java
@@ -1,12 +1,18 @@
 package com.jenkins;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication extends SpringServletContainerInitializer {
+@Configuration
+@EnableAutoConfiguration
+public class JenkinsTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTestApplication.class, args);
